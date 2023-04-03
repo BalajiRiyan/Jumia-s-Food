38eafbb6233be4f9223ec717e8b07f68d03560b2
dell <dell@BALAJI>
2023-04-03 16:29:27 +0530

    one more method added in Testclass3.

--- src/test/java/Tc_03Test.java
+++ src/test/java/Tc_03Test.java
@@ -9,6 +9,10 @@ public class Tc_03Test {
 	public void testMethod6() {
 		System.out.println("build created-----06");
 	}
+	@Test(groups = "smoke")
+	public void testMethod7() {
+		System.out.println("build created-----07");
+	}
 
 
 }
