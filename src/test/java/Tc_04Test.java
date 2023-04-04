73df0cea19c79f7289bd4ebb0f629e0d653c3dcc
dell <dell@BALAJI>
2023-04-04 11:02:13 +0530

    ok

---
+++ src/test/java/Tc_04Test.java
@@ -0,0 +1,14 @@
+import org.testng.annotations.Test;
+
+public class Tc_04Test {
+	@Test(groups = "smoke")
+	public void testMethod() {
+		System.out.println("build created-----");
+	}
+	@Test(groups = "regression")
+	public void testMethod2() {
+		System.out.println("build created-----");
+	}
+
+
+}
